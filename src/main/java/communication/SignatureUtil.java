d6828be1472110c3ea289b193f569984a9c31f84
Elias Brunner <elias_brunner@gmx.ch>
2018-11-10 12:57:57 +0100

    new file

---
+++ src/main/java/communication/SignatureUtil.java
@@ -0,0 +1,9 @@
+package communication;
+
+public class SignatureUtil {
+
+    // getFileSignature ähnlich wie getKeySignature von EncryptionUtil
+    // return Byte[32]
+    // SHA256
+    // richtige Tab-Order in LoginScreen
+}
