81adc474fef9125417f7a908994a661fe881638b
lexli8888 <alexander.van.schie@hsr.ch>
2018-11-19 17:31:54 +0100

    FileChooser functionality

--- src/main/java/gui/controller/AddNotariatFileController.java
+++ src/main/java/gui/controller/AddNotariatFileController.java
@@ -2,23 +2,47 @@ package gui.controller;
 
 import gui.Main;
 import gui.state.DataState;
+import gui.util.AlertFactory;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class AddNotariatFileController implements IDataStateModalController {
 
+    final FileChooser fileChooser = new FileChooser();
+    private File file = null;
     private Main mainapp;
     private DataState dataState;
     private Stage dialogStage;
     private boolean isOkClicked = false;
 
     @FXML
-    public void handleDatei() {
+    private TextField filepath;
 
+    @FXML
+    public void handleDatei() {
+        if(file != null){
+
+        }
+        else{
+            Alert alert = AlertFactory.ErrorAlert("No file", "Wähle eine Datai aus um fortzufahren!");
+            alert.initOwner(mainapp.getPrimaryStage());
+            alert.showAndWait();
+        }
     }
 
     @FXML
     public void handleBrowse() {
+        file = null;
+        filepath.clear();
+        file = fileChooser.showOpenDialog(mainapp.getPrimaryStage());
+        if (file != null) {
+           filepath.setText(file.getPath());
+        }
     }
 
     @Override
